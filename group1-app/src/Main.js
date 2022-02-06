995c48c1b4f064f6b880ed1210998bd0eac654f4
Shaira Alam <shaira.alam19@gmail.com>
2022-02-05 19:48:35 -0700

    react app

---
+++ group1-app/src/Main.js
@@ -0,0 +1,16 @@
+import './roxy.css';
+
+function Main() {
+    return (
+      <div className="App-header">
+        <h1>Title!</h1>
+        <p>
+            Hello World! 
+            <br></br>
+            className = App-Header -&gt; affects the CSS (try removing it!)
+        </p>
+      </div>
+    );
+  }
+  
+  export default Main;
