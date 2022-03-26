8eaf6c8e5c138910b373c3697979b512213897db
Leyli Ghavami <leylighavami@gmail.com>
2022-03-26 12:53:07 -0700

    Added Navbar & Button components

---
+++ group-1-app/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
