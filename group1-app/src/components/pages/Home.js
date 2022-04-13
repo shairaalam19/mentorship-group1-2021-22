feb8fddcdb4d01afb081be904a60b97db010b874
cosecE <97705220+cosecE@users.noreply.github.com>
2022-04-12 17:47:25 -0700

    footer

--- group1-app/src/components/pages/Home.js
+++ group1-app/src/components/pages/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Cards from '../Cards';
+import Footer from '../Footer';
 import "../../App.css";
 import { HeroSection } from "../HeroSection";
 
@@ -6,6 +8,8 @@ export function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
+      <Footer />
     </>
   );
 }
