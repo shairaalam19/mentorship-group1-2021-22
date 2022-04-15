6a8ef7b0c960b9020696bcd84ed8d4d217711445
cosecE <97705220+cosecE@users.noreply.github.com>
2022-04-15 15:42:01 -0700

    homepage

--- group1-app/src/components/HeroSection.js
+++ group1-app/src/components/HeroSection.js
@@ -7,13 +7,13 @@ export function HeroSection() {
   return (
     <div className='hero-container'>
         <video src ='/videos/video-3.mp4' autoPlay loop muted />  
-        <h1>FIND YOURSELF</h1>
-        <p> What are you waiting for?</p>
+        <h1>HELLO WORLD</h1>
+        <p> Pursue your happiness</p>
           <div className='hero-btns'>
-           <Button className = 'btns' buttonStyle = 'btn--outline' 
+           <Button className = 'btn' buttonStyle = 'btn--outline' 
             buttonSize='btn--large'> GET STARTED </Button> 
         
-       <Button className = 'btns' buttonStyle = 'btn--primary' 
+       <Button className = 'btn' buttonStyle = 'btn--primary' 
              buttonSize='btn--large'> WATCH TRAILER <i className = 'far fa-play-circle' />
             
             </Button>  
--- group1-app/src/components/Navbar.js
+++ group1-app/src/components/Navbar.js
@@ -34,7 +34,7 @@ return (
     <nav className='navbar'>
        <div className='navbar-container'>
         <Link to='/' className='navbar-logo' onClick={closeMobileMenu} >
-            TRVL <i className='fab fa-typo3' />
+            HW <i className='fab fa-typo3' />
         </Link> 
         <div className='menu-icon' onClick={handleClick}>
         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
